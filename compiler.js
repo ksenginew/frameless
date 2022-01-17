9ca935b2102aa5a84c6b3aed364ee5ea432e239d
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-17 15:55:31 +0530

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -1,46 +1,15 @@
-let SERVERRE = /<\s*script(\s+[^]*?)?\sserver(\s+[^]*?)?>([^]*?)<\/\s*script\s*>/g
+let SERVERRE =
+  /<\s*script(\s+[^]*?)?\sserver(\s+[^]*?)?>([^]*?)<\/\s*script\s*>/g;
 
 export function compiler(source) {
-  let server = ''
+  let server = "";
   let static = source.replace(SERVERRE, (_, $1, $2, $3) => {
-    server += $2 + '\n'
-    return ''
-  })
-  return { server, static }
+    server += $2 + "\n";
+    return "";
+  });
+  return { server, static };
 }
-
-let HTMLRE =
-  /<(\/?)([^\s/>]*)|\s+([^\s/>]+)(?:\s*=\s*("(?:\\"|[^"])*"|[^\s/>]+))?|(\/?)>|([^<]+)/g
-
-let html = (strings, ...args) => {
-  let src = strings.reduce(
-    (previous, current, index) => previous + current + '{%' + index + '%}',
-    ''
-  )
-
-  let isTag = 0
-  let tagname
-  let attrMap = {}
-  let tree = [[]]
-  while ((m = HTMLRE.exec(src))) {
-    tokens.push(m[0])
-    if(!isTag && m[6])
-        tree[0].push()
-    else if (isTag && m[5])
-        tree.unshift([tagname, attrMap])
-    else if (isTag && m[3])
-        attrMap[m[3]] = m[4]
-    else if (!isTag && m[1])
-        tree.
-    else
-      tree[0]
-  }
-
-  return tokens
-}
-
-export let ctx = { html }
 
 export function ssrJs(server, static) {
-  return new Function('__', server+'return __.html`' + static + '`;')
+  return new Function("__", server + "return `" + static + "`;");
 }
