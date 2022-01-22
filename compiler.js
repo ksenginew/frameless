4cda31929b5b4c058d43243f931db07199036d83
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-22 09:56:38 +0530

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -4,14 +4,14 @@ let RE =
 export function compiler(source) {
   let server = "";
   let style = "";
-  let static = source.replace(SCRIPT, (_, $1, $2, $3, $4) => {
+  let html = source.replace(SCRIPT, (_, $1, $2, $3, $4) => {
     if($2)
       server += $2 + "\n";
     if($4)
       style += $4;
     return "";
   });
-  return { server, static, style };
+  return { server, html, style };
 }
 
 export function ssrJs(server, static) {
