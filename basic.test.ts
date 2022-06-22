56e41eb0aa4a802de967e96ad130faeecb7af864
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-06-22 12:59:13 +0530

    commit

--- basic.test.ts
+++ basic.test.ts
@@ -1,10 +1,10 @@
-import {globbySync} from 'globby';
+import glob from 'glob';
 import {compiler, ssrJs} from './compiler';
 import {readFileSync} from 'fs'
 
 describe('test', () => {
   test('all', () => {
-    globbySync('examples/**/App.html').map(file => {
+    glob.sync('examples/**/App.html').map(file => {
       let { server, html, style } = compiler(readFileSync(file).toString())
       expect(`
       // file: ${file}
