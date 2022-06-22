790dea81d61f5ccbf80b76310958ddc4995025ab
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-06-22 13:14:46 +0530

    commit

--- basic.test.ts
+++ basic.test.ts
@@ -1,19 +1,20 @@
 import glob from 'glob';
-import {compiler, ssrJs} from './compiler';
-import {readFileSync} from 'fs'
+import { compiler, ssrJs } from './compiler';
+import { readFileSync } from 'fs'
 
 describe('test', () => {
   test('all', () => {
     glob.sync('examples/**/App.html').map(file => {
       let { server, html, style } = compiler(readFileSync(file).toString())
       expect(`
-      // file: ${file}
-      function App() {
-        ${server}
-
-        return \`${html}\`
-      }
-      `).toMatchSnapshot()
+// file: ${file}
+------------------------------
+js   = ${server}
+------------------------------
+css  = ${style}
+------------------------------
+html =${html}
+`).toMatchSnapshot()
     })
   })
 })
