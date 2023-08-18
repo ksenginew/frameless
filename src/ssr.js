b4bc530cb6ea762de0c4b5810384915d72018204
ksenginew <ksengine.github@gmail.com>
2023-08-18 15:47:54 +0530

    commit

--- src/ssr.js
+++ src/ssr.js
@@ -6,8 +6,25 @@ const void_element_names =
 // @ts-ignore
 let esc = str => String(str).replace(/[&<>"']/g, s => `&${map[s]};`);
 let map = { '&': 'amp', '<': 'lt', '>': 'gt', '"': 'quot', "'": 'apos' };
+/** @type {Record<string,string>} */
 let sanitized = {};
 
-export function renderToString() {
-  
+/**
+ * @param {import("./types").FrElement|import("./types").FrElement[]} element
+ * @returns {string}
+ */
+export function renderToString(element) {
+  if (!element) return ""
+  else if (Array.isArray(element)) return element.map(e => renderToString(e, slots)).join('')
+  else if (typeof element === "string") return sanitized[element] || (sanitized[element] = esc(element))
+  else if (typeof element.type == "symbol") return renderToString(element.props?.children, slots)
+  else if (typeof element.type == "function") return renderToString(element.type({ props: element.props || {} }), element?.props?.children)
+  else {
+    let { children, ...args = {} } = element.props || {}
+    let tagName = element.type.toLowerCase()
+    let is_void = void_element_names.test(tagName)
+    if (tagName == "slot") 
+    return renderToString(slots||children,[])
+    return "<" + tagName + Object.entries(args).map(([arg, value]) => " " + esc(arg) + '"' + esc(value) + "'")+" > " +content
+  }
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,42 +1,9 @@
-export interface Node {
-  type: string;
-  start: number;
-  end: number;
+export type FrComponent = (props: Record<string, *>) => FrElement
+export interface FrElement {
+  $$typeof: symbol;
+  type: string | FrComponent | symbol;
+  key: any;
+  ref: string;
+  props: Record<string, *>;
+  _owner: any;
 }
-
-export interface Comment extends Node {
-  type: "comment";
-  data: string;
-}
-
-export interface Text extends Node {
-  type: "text";
-  data: string;
-}
-
-export interface Raw extends Node {
-  type: "raw";
-  data: string;
-}
-
-export interface Template extends Node {
-  type: "template";
-  data: string;
-}
-
-export interface VoidElement extends Node {
-  type: "element";
-  name: string;
-  attrs: Record<string, string | Template | undefined>;
-}
-
-export interface ContentElement extends VoidElement {
-  data: string;
-}
-
-export interface Element extends VoidElement {
-  data: number[];
-  attrs: Record<string, string | Template | undefined>;
-}
-
-export type NodeLike = Comment | Text | Template | Element;
