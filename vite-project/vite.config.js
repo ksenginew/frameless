8f9207cd431e4e76e015a147cfb33f7c3cc7301c
ksenginew <ksengine.github@gmail.com>
2023-09-02 12:50:43 +0530

    commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  (() => {
+    let vite;
+    return {
+      name: 'ssr',
+      resolveId(id,importter,{isEntry}){
+        // if(isEntry)
+          console.log(id)
+      },
+      transform(id,ctx){
+        console.log(ctx)
+      },
+      load(id,{ssr}){
+        console.log(id)
+        if(id.endsWith(".html")) console.log(id)
+      },
+      configureServer(server) {
+        vite = server
+        // server.middlewares.use(async (req, res, next) => {
+        //   const url = req.originalUrl
+        //   console.log(req.url, url)
+        //   const { render } = await server.ssrLoadModule('/src/entry-server.js')
+        //   res.writeHead(200, undefined, { 'Content-Type': 'text/html' }).end(render())
+        // })
+      }
+    }
+  })()
+  ],
+  build: {
+    minify: false,
+  },
 })
